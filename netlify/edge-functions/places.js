a34d0853a4b7d50e35e0582f9366863f086f2ca6
Julie <113617761+julieasoreng@users.noreply.github.com>
2026-03-30 11:41:32 +0200

    debug: check env variable

--- netlify/edge-functions/places.js
+++ netlify/edge-functions/places.js
@@ -1,34 +1,11 @@
 export default async (request, context) => {
-  const url = new URL(request.url);
-  const query = url.searchParams.get("query");
-
-  if (!query) {
-    return new Response(JSON.stringify({ error: "Mangler søk" }), {
-      status: 400,
-      headers: { "Content-Type": "application/json" }
-    });
-  }
-
   const apiKey = Deno.env.get("GOOGLE_PLACES_KEY");
-
-  const googleUrl = `https://places.googleapis.com/v1/places:searchText`;
-
-  const response = await fetch(googleUrl, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Goog-Api-Key": apiKey,
-      "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.websiteUri,places.rating,places.userRatingCount,places.id"
-    },
-    body: JSON.stringify({ textQuery: query })
-  });
-
-  const data = await response.json();
-
-  return new Response(JSON.stringify(data), {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
-    }
+  
+  return new Response(JSON.stringify({ 
+    keyExists: !!apiKey,
+    keyLength: apiKey ? apiKey.length : 0,
+    keyStart: apiKey ? apiKey.substring(0, 6) : "ingen"
+  }), {
+    headers: { "Content-Type": "application/json" }
   });
 };
